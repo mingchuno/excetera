9fae1afa02a2ba3105810573329bc5b08cfabc8f
mingchun.or <mingchuno@gmail.com>
2016-04-07 17:45:58 +0800

    publish to hex.pm

--- mix.exs
+++ mix.exs
@@ -1,13 +1,29 @@
 defmodule Excetera.Mixfile do
   use Mix.Project
 
+  @version "0.0.2"
+
   def project do
     [
       app: :excetera,
-      version: "0.0.2",
-      elixir: "~> 1.2.0",
-      deps: deps
+      version: @version,
+      elixir: "~> 1.2",
+      description: description,
+      package: package,
+      source_url: "https://github.com/mingchuno/excetera",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps,
+      docs: [extras: ["README.md"], main: "readme",
+             source_ref: "v#{@version}",
+             source_url: "https://github.com/mingchuno/excetera"]
     ]
+  end
+
+  defp description do
+    """
+    Elixir bindings for etcd's HTTP API.
+    """
   end
 
   def application do
@@ -21,7 +37,17 @@ defmodule Excetera.Mixfile do
   defp deps do
     [
       {:httpoison, "~> 0.8.0"},
-      {:poison, "~> 2.0"}
+      {:poison, "~> 2.0"},
+      {:earmark, "~> 0.2", only: :docs},
+      {:ex_doc, "~> 0.11", only: :docs},
+      {:inch_ex, "~> 0.5", only: :docs}
     ]
+  end
+
+  defp package do
+    [files: ["lib", "mix.exs", "README.md", "LICENSE"],
+     maintainers: ["mingchuno"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/mingchuno/excetera"}]
   end
 end
